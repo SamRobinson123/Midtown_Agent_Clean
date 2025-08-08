e3308f0815e3f7ef3f2d1a961f37adae68dee515
SamRobinson123 <156377261+SamRobinson123@users.noreply.github.com>
2025-08-08 15:00:00 -0600

    Update main.tsx

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import ChatWidget from "./ChatWidget";
+import "./index.css";
 
-const root = createRoot(document.getElementById("root")!);
-root.render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div style={{ display: "flex", justifyContent: "center", padding: 16 }}>
       <ChatWidget />
